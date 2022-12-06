28959d7cba716958dba2f3f554c41c4e4a11d881
anhkiet210 <anhkietk053@gmail.com>
2022-12-07 02:51:26 +0700

    change config file vercel.json

--- src/app/http/middlewares/authAdmin.middleware.js
+++ src/app/http/middlewares/authAdmin.middleware.js
@@ -9,22 +9,16 @@ const isAuthAdmin = async (req, res, next) => {
             message: 'Vui lòng đăng nhập để truy cập tài nguyên này!',
         });
     }
-    // console.log({ tokenFromClient });
 
-    try {
-        const isVerifyToken = await jwtHelper.verifyToken(tokenFromClient);
-        if (!isVerifyToken) {
-            return res.status(401).json({
-                success: false,
-                message: 'Bạn không có quyền truy cập tính năng này!',
-            });
-        }
-        req.user = await AdminModel.findById(isVerifyToken.id);
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ success: false, message: 'Phiên đăng nhập của bạn đã hết hạn, hãy đăng nhập lại' });
+    const isVerifyToken = jwtHelper.verifyToken(tokenFromClient);
+    if (!isVerifyToken) {
+        return res.status(401).json({
+            success: false,
+            message: 'Bạn không có quyền truy cập tính năng này!',
+        });
     }
+    req.user = await AdminModel.findById(isVerifyToken.id);
+    return next();
 };
 
 export default isAuthAdmin;
